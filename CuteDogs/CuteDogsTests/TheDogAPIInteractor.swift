38923c6b021ab920634cec06f02fa57d2cffc09d
Victor Sousa <victorcm.sousa@gmail.com>
2022-12-16 15:50:03 +0000

    Adding more unit test for DogApi Interactor

--- CuteDogs/CuteDogsTests/TheDogAPIInteractor.swift
+++ CuteDogs/CuteDogsTests/TheDogAPIInteractor.swift
@@ -28,6 +28,51 @@ final class TheDogAPIInteractorTests: XCTestCase {
         XCTAssertEqual(received.query?.contains("limit=0"), true, "pageSize")
     }
     
+    func test_fetchWeather_mapCuteDogs() async throws {
+
+        let clientSpy = HTTPClientSpy()
+        let sut = makeSUT(client: clientSpy)
+        clientSpy.getUrlStub = (.fiveBreeds, nil)
+
+        let cuteDogs = try await sut.fetchBreeds(size: 5, pageNumber: 0)
+
+        XCTAssertEqual(cuteDogs.count, 5)
+        XCTAssertEqual(cuteDogs.first!.breedName, "Affenpinscher")
+        XCTAssertEqual(cuteDogs.first!.origin, "Germany, France")
+        XCTAssertEqual(cuteDogs.first!.breedGroup, "Toy")
+        XCTAssertEqual(cuteDogs.first!.imageURL!.absoluteString, "https://cdn2.thedogapi.com/images/BJa4kxc4X.jpg")
+    }
+    
+    func test_fetchWeather_decodeErrorOnEmptyData() async throws {
+
+        let clientSpy = HTTPClientSpy()
+        let sut = makeSUT(client: clientSpy)
+        clientSpy.getUrlStub = (Data(), nil)
+
+        do {
+            
+            let _ = try await sut.fetchBreeds(size: 5, pageNumber: 0)
+            XCTFail("Expecting failure")
+        } catch let error as ApiError {
+            XCTAssertEqual(error, .decodeError)
+        }
+    }
+    
+    func test_fetchWeather_apiErrorOnClient() async throws {
+
+        let clientSpy = HTTPClientSpy()
+        let sut = makeSUT(client: clientSpy)
+        clientSpy.getUrlStub = (Data(), ApiError.apiError)
+
+        do {
+            
+            let _ = try await sut.fetchBreeds(size: 5, pageNumber: 0)
+            XCTFail("Expecting failure")
+        } catch let error as ApiError {
+            XCTAssertEqual(error, .apiError)
+        }
+    }
+    
     func makeSUT(client: HTTPClient = HTTPClientSpy(),
                  file: StaticString = #filePath,
                  line: UInt = #line) -> BreedsInteractor {
