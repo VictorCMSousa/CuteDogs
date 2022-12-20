8618d9f2fa99fa91c90f37f662ae0df78b3da01e
Victor Sousa <victorcm.sousa@gmail.com>
2022-12-20 09:18:59 +0000

    Use File system to handle image cache

--- CuteDogs/CuteDogs/Interfaces/ImageCache.swift
+++ CuteDogs/CuteDogs/Interfaces/ImageCache.swift
@@ -10,17 +10,34 @@ import Foundation
 final class ImageCacheLoader: ImageLoaderInteractor, ImageCache {
     
     private let myQueue = DispatchQueue(label: "my.imageCache", attributes: .concurrent)
-    private var cache = [URL: Data]()
     
     func fetchImage(imageURL: URL) async throws -> Data? {
         myQueue.sync {
-            cache[imageURL]
+            try? imageFromFileSystem(for: imageURL)
         }
     }
     
     func save(_ data: Data, for url: URL) {
         myQueue.async(flags: .barrier) { [weak self] in
-            self?.cache[url] = data
+            try? self?.persistImage(data, url: url)
         }
+    }
+    private func imageFromFileSystem(for urlRequest: URL) throws -> Data? {
+        guard let url = fileName(for: urlRequest) else { return nil }
+
+        return try Data(contentsOf: url)
+    }
+    
+    private func persistImage(_ data: Data, url: URL) throws {
+        guard let filename = fileName(for: url) else { return }
+        try data.write(to: filename)
+    }
+    
+    private func fileName(for url: URL) -> URL? {
+        guard let fileName = url.lastPathComponent.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed),
+              let applicationSupport = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first else {
+            return nil
+        }
+        return applicationSupport.appendingPathComponent(fileName)
     }
 }
